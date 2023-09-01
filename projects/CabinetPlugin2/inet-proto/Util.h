059da6b62c47892796705f45841ef72ccca315a8
omggga <omggga@gmail.com>
2023-09-01 10:07:33 +0300

    util

---
+++ projects/CabinetPlugin2/inet-proto/Util.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#ifdef _DEBUG
+#pragma message("Including: " __FILE__)
+#endif
+
+// ����������������� [] (������ ���� �������� ������ ������ ������)
+
+#include "ProtocolDefs.h"
+
+// �������� ���������� ����� ��������� ����������: 
+
+#include "../_cpp/SlotUtil.h"
+	//
+	// #include <low-level/SmartInf.h> // ���� low-level
+
+
+
+
+
+
+namespace  {
+
+	namespace Protocol {
+
+	} // namespace Protocol
+
+} // namespace  
