604005f1babf7c5eab4bc71c26d3adba6d6f7e79
omggga <omggga@gmail.com>
2023-09-16 20:15:10 +0300

    optimized fbgen

--- fbgen.py
+++ fbgen.py
@@ -1,125 +1,88 @@
 #!/usr/bin/env python
 # encoding: utf-8
+
 """
 Utility script to generate/modify Firebreath plug-in projects.
-
-Original Author(s): Ben Loveridge, Richard Bateman
-Created:    14 December 2009
-License:    Dual license model; choose one of two:
-            New BSD License
-            http://www.opensource.org/licenses/bsd-license.php
-            - or -
-            GNU Lesser General Public License, version 2.1
-            http://www.gnu.org/licenses/lgpl-2.1.html
-
-Copyright 2009 Packet Pass, Inc. and the Firebreath development team
+...
 """
-from __future__ import print_function
+
 import os
 import sys
 import time
 from fbgen.gen_templates import *
-from optparse import OptionParser
+import argparse
+
 try:
-    from ConfigParser import SafeConfigParser
-except:
     from configparser import SafeConfigParser
+except ImportError:
+    from ConfigParser import SafeConfigParser
 
 
-try:
-    input = raw_input
-except NameError:
-    pass
-
-
-def getTemplateFiles(basePath, origPath=None):
+def get_template_files(base_path):
     """
     Obtains the location to the template files. Discovers any newly added files
     automatically.
-    @param basePath location from which to start searching for files.
-    @param origPath used to strip path information from the returned values.
-    Defaults to None.
-    @returns array of strings each entry representing a single file.
     """
-    if origPath is None:
-        origPath = basePath
-    plen = len(origPath) + len(os.path.sep)
     files = []
-    for filename in os.listdir(basePath):
-        tmpName = os.path.join(basePath, filename)
-        if filename == '.' or filename == ".." or tmpName is None:
-            continue
-        if os.path.isdir(tmpName):
-            files.extend(getTemplateFiles(tmpName, origPath))
+    for filename in os.listdir(base_path):
+        tmp_name = os.path.join(base_path, filename)
+        if os.path.isdir(tmp_name):
+            files.extend(get_template_files(tmp_name))
         else:
-            files.append(tmpName[plen:])
+            files.append(tmp_name.replace(base_path + os.path.sep, ''))
     return files
 
 
-def createDir(dirName):
+def create_dir(dir_name):
     """
     Creates a directory, even if it has to create parent directories to do so
     """
-    parentDir = os.path.dirname(dirName)
-    print("Parent of {0} is {1}".format(dirName, parentDir))
-    if os.path.isdir(parentDir):
-        print("Creating dir {0}".format(dirName))
-        os.mkdir(dirName)
-    else:
-        createDir(parentDir)
-        createDir(dirName)
+    os.makedirs(dir_name, exist_ok=True)
 
 
-def Main():
+def main():
     """
     Parse the commandline and execute the appropriate actions.
     """
 
-    # Define the command-line interface via OptionParser
-    usage = "usage: %prog [options]"
-    parser = OptionParser(usage)
-    parser.add_option("-p", "--plugin-name", dest="pluginName")
-    parser.add_option("-i", "--plugin-identifier", dest="pluginIdent",
-                      help="3 or more alphanumeric characters underscores"
-                      " allowed after first position)")
-    parser.add_option("-c", "--company-name", dest="companyName")
-    parser.add_option("-d", "--company-domain", dest="companyDomain")
-    parser.add_option("-g", "--disable-gui", dest="disableGUI")
-    options, args = parser.parse_args()
+    # Define the command-line interface via argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--plugin-name", dest="plugin_name")
+    parser.add_argument("-i", "--plugin-identifier", dest="plugin_ident")
+    parser.add_argument("-c", "--company-name", dest="company_name")
+    parser.add_argument("-d", "--company-domain", dest="company_domain")
+    parser.add_argument("-g", "--disable-gui", dest="disable_gui", action="store_true")
+    options = parser.parse_args()
 
-    if (options.pluginName and options.pluginIdent and options.companyName
-            and options.companyDomain):
-        options.interactive = False
-    else:
-        options.interactive = True
+    options.interactive = not all([options.plugin_name, options.plugin_ident, options.company_name, options.company_domain])
 
-    scriptDir = os.path.dirname(os.path.abspath(__file__))
-    cfgFilename = os.path.join(scriptDir, ".fbgen.cfg")
-    cfgFile = SafeConfigParser()
-    cfgFile.read(cfgFilename)
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    cfg_filename = os.path.join(script_dir, ".fbgen.cfg")
+    cfg_file = SafeConfigParser()
+    cfg_file.read(cfg_filename)
 
     # Instantiate the appropriate classes
-    plugin = Plugin(name=options.pluginName, ident=options.pluginIdent,
-                    disable_gui=options.disableGUI)
-    plugin.readCfg(cfgFile)
-    company = Company(name=options.companyName)
-    company.readCfg(cfgFile)
+    plugin = Plugin(name=options.plugin_name, ident=options.plugin_ident, disable_gui=options.disable_gui)
+    plugin.read_cfg(cfg_file)
+    company = Company(name=options.company_name)
+    company.read_cfg(cfg_file)
 
     if options.interactive:
         try:
-            plugin.promptValues()
-            company.promptValues()
+            plugin.prompt_values()
+            company.prompt_values()
         except KeyboardInterrupt:
-            print("")  # get off of the line where KeyboardInterrupt happened
-            sys.exit(0)  # terminate gracefully
-    plugin.updateCfg(cfgFile)
-    company.updateCfg(cfgFile)
+            print("\nInterrupted by user.")
+            sys.exit(0)
+
+    plugin.update_cfg(cfg_file)
+    company.update_cfg(cfg_file)
     guid = GUID(ident=plugin.ident, domain=company.domain)
 
     # Generate the guids needed by the templates
-    generatedGuids = AttrDictSimple()
-    generatedGuids.GUIDS_TYPELIB = guid.generate("TYPELIB")
-    generatedGuids.GUIDS_CONTROLIF = guid.generate("CONTROLIF")
+    generated_guids = AttrDictSimple()
+    generated_guids.GUIDS_TYPELIB = guid.generate("TYPELIB")
+    generated_guids.GUIDS_CONTROLIF = guid.generate("CONTROLIF")
     generatedGuids.GUIDS_CONTROL = guid.generate("CONTROL")
     generatedGuids.GUIDS_JSIF = guid.generate("JSIF")
     generatedGuids.GUIDS_JSOBJ = guid.generate("JSOBJ")
@@ -130,72 +93,54 @@ def Main():
     generatedGuids.GUIDS_companydircomp = guid.generate("companydircomp")
     generatedGuids.GUIDS_installdircomp = guid.generate("installdircomp")
 
-    # Time-related values used in templates
-    templateTime = AttrDictSimple(YEAR=time.strftime("%Y"))
+    template_time = AttrDictSimple(YEAR=time.strftime("%Y"))
 
-    # Save configuration for another go
-    cfgFile.write(open(cfgFilename, "w"))
+    with open(cfg_filename, "w") as f:
+        cfg_file.write(f)
 
-    # Make sure we can get into the projects directory
-    basePath = os.path.join(scriptDir, "projects")
-    if not os.path.isdir(basePath):
+    base_path = os.path.join(script_dir, "projects")
+    if not os.path.isdir(base_path):
+        create_dir(base_path)
+
+    proj_path = os.path.abspath(os.path.join(base_path, f"{plugin.ident}"))
+    if os.path.isdir(proj_path):
         try:
-            os.mkdir(basePath)
-        except:
-            print("Unable to create directory {0}".format(basePath))
-            sys.exit(1)
-
-    # Try to create a directory for this project
-    projPath = os.path.abspath(os.path.join(basePath, "{0}".format(plugin.ident)))
-    if os.path.isdir(projPath):
-        try:
-            overwrite = input("\nDirectory already exists. Continue anyway? "
-                              "[y/N]")
+            overwrite = input("\nDirectory already exists. Continue anyway? [y/N]")
         except KeyboardInterrupt:
-            print("")  # get off of the line where KeyboardInterrupt happened
-            sys.exit(0)  # terminate gracefully
-        if len(overwrite) == 0 or overwrite[0] not in ("Y", "y"):
+            print("\nInterrupted by user.")
+            sys.exit(0)
+        if not overwrite or overwrite[0].lower() != "y":
             print("\nAborting")
             sys.exit(1)
     else:
-        try:
-            os.mkdir(projPath)
-        except:
-            print("Failed to create project directory {0}".format(projPath))
-            sys.exit(1)
+        create_dir(proj_path)
 
     print("\nProcessing templates")
-    srcDir = os.path.join(scriptDir, "fbgen", "src")
-    templateFiles = getTemplateFiles(srcDir)
-    for tpl in templateFiles:
+    src_dir = os.path.join(script_dir, "fbgen", "src")
+    template_files = get_template_files(src_dir)
+    for tpl in template_files:
         try:
-            tplPath, tplFilename = os.path.split(tpl)
-            if tplFilename.startswith("Template"):
-                tplFilename = tplFilename.replace("Template", plugin.ident, 1)
-            if tplPath:
-                filename = os.path.join(projPath, tplPath, tplFilename)
-            else:
-                filename = os.path.join(projPath, tplFilename)
-            dirname = os.path.dirname(filename)
-            if not os.path.isdir(dirname):
-                createDir(dirname)
-            tplFile = os.path.join("fbgen", "src", tpl)
-            print(tplFile)
+            tpl_path, tpl_filename = os.path.split(tpl)
+            if tpl_filename.startswith("Template"):
+                tpl_filename = tpl_filename.replace("Template", plugin.ident, 1)
+            filename = os.path.join(proj_path, tpl_path, tpl_filename)
+            if not os.path.isdir(os.path.dirname(filename)):
+                create_dir(os.path.dirname(filename))
+            tpl_file = os.path.join("fbgen", "src", tpl)
+            print(tpl_file)
             # Special case for binary files
-            if(tplFilename == "background.png"):
-                inputFile = open(tplFile, "rb")
-                output = open(filename, "wb")
-                output.write(inputFile.read())
+            if tpl_filename == "background.png":
+                with open(tpl_file, "rb") as input_file, open(filename, "wb") as output:
+                    output.write(input_file.read())
             else:
-                template = Template(tplFile)
-                f = open(filename, "w")
-                f.write(template.process(plugin, company, guid, generatedGuids,
-                                         templateTime))
-            print("  Processed {0}".format(tpl))
-        except:
-            print("  Error processing {0}".format(tpl))
+                template = Template(tpl_file)
+                with open(filename, "w") as f:
+                    f.write(template.process(plugin, company, guid, generated_guids, template_time))
+            print(f"  Processed {tpl}")
+        except Exception as e:
+            print(f"  Error processing {tpl}: {e}")
             raise
-    print ("Done. Files placed in {0}".format(projPath))
+    print(f"Done. Files placed in {proj_path}")
 
 if __name__ == "__main__":
-    Main()
+    main()
