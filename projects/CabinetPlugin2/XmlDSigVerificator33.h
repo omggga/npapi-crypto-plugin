00105d8a0cc3692f17da425bf355118a06e556b8
omggga <omggga@gmail.com>
2023-09-17 16:14:12 +0300

    wip

--- projects/CabinetPlugin2/XmlDSigVerificator33.h
+++ projects/CabinetPlugin2/XmlDSigVerificator33.h
@@ -2,19 +2,21 @@
 using .ASN;
 using .Algorithms;
 
-namespace 
+namespace
 {
-    class XmlDSigVerificator33 : IVerificator
+    public class XmlDSigVerificator33 : IVerificator
     {
         public bool VerifySignature(byte[] signatureCMS, byte[] data, byte[] certificate)
         {
-            AsnObject m = new AsnObject(signatureCMS);
-            byte[] signature = m[1][0][3][0][4].Data;
-            //
-            byte[] pubKey = XmlDSigHelper33.GetPublicKeyOfCert(certificate);
-            //
-            Stream dataStream = new EnvelopingStream(data);
-            return XmlDSigHelper33.VerifySignature(pubKey, dataStream, signature) > 0;
+            var asnObject = new AsnObject(signatureCMS);
+            byte[] signature = asnObject[1][0][3][0][4].Data;
+
+            byte[] publicKey = XmlDSigHelper33.GetPublicKeyOfCert(certificate);
+
+            using (Stream dataStream = new EnvelopingStream(data))
+            {
+                return XmlDSigHelper33.VerifySignature(publicKey, dataStream, signature) > 0;
+            }
         }
     }
 }
